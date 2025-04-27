db95d539de50fdd8fe16829670fa363805100ef3
regis066 <mugisharegis72@gmail.com>
2025-04-27 22:14:56 +0200

    feat(model): Add Message model

---
+++ src/main/java/com/java_ne/banking/model/Message.java
@@ -0,0 +1,27 @@
+package com.java_ne.banking.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Customer customer;
+
+    @Column(columnDefinition = "TEXT")
+    private String message;
+    private LocalDateTime dateTime;
+}
