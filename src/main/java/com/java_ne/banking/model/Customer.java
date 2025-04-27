341a5516632c21f50dbeaafa7f337690a019bf8a
regis066 <mugisharegis72@gmail.com>
2025-04-27 22:14:43 +0200

    feat(model): Add Customer model

---
+++ src/main/java/com/java_ne/banking/model/Customer.java
@@ -0,0 +1,32 @@
+package com.java_ne.banking.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String email;
+    private String lastName;
+    private String mobile;
+    private LocalDate dob;
+    private String account;
+    private Double balance;
+    private LocalDateTime LastUpdateDateTime;
+
+}
