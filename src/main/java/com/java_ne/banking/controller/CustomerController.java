b0f3243641b07e6608c6d42066a92399e28dcf1e
regis066 <mugisharegis72@gmail.com>
2025-05-01 19:54:51 +0200

    refactor: modify status code returned after customer registration from 200 to 201

--- src/main/java/com/java_ne/banking/controller/CustomerController.java
+++ src/main/java/com/java_ne/banking/controller/CustomerController.java
@@ -1,11 +1,11 @@
 package com.java_ne.banking.controller;
 
 
-import com.java_ne.banking.dto.CustomerRequest;
+import com.java_ne.banking.dto.CustomerRequestDTO;
 import com.java_ne.banking.model.Customer;
 import com.java_ne.banking.service.CustomerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,9 +19,9 @@ public class CustomerController {
     private final CustomerService customerService;
 
     @PostMapping
-    public ResponseEntity<Customer> createCustomer(@RequestBody CustomerRequest request) {
+    public ResponseEntity<Customer> createCustomer(@RequestBody CustomerRequestDTO request) {
         Customer customer = customerService.createCustomer(request);
-        return ResponseEntity.ok(customer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(customer);
     }
 
 }
