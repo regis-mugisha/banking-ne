196013430796e8284f7d881b3fff2fb81fd30757
regis066 <mugisharegis72@gmail.com>
2025-05-05 10:13:35 +0200

    feat: send email after save, withdraw or transfer transaction

--- src/main/java/com/java_ne/banking/service/BankingService.java
+++ src/main/java/com/java_ne/banking/service/BankingService.java
@@ -1,11 +1,15 @@
 package com.java_ne.banking.service;
 
 import com.java_ne.banking.dto.BankingRequestDTO;
+import com.java_ne.banking.exception.AccountNotFoundException;
+import com.java_ne.banking.exception.InsufficientBalanceException;
+import com.java_ne.banking.exception.InvalidTransactionTypeException;
 import com.java_ne.banking.model.Banking;
 import com.java_ne.banking.model.Customer;
 import com.java_ne.banking.model.TransactionType;
 import com.java_ne.banking.repository.BankingRepository;
 import com.java_ne.banking.repository.CustomerRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,14 +20,16 @@ import java.time.LocalDateTime;
 public class BankingService {
     private final BankingRepository bankingRepository;
     private final CustomerRepository customerRepository;
+    private final MessageService messageService;
 
 
     public Banking saveWithdraw(BankingRequestDTO request) {
         Customer customer =
-                customerRepository.findByAccount(request.getAccount()).orElseThrow(() -> new RuntimeException(
-                        "Account doesn't exist"));
+                customerRepository.findByAccount(request.getAccount()).orElseThrow(() -> new AccountNotFoundException(
+                        "Account not found"));
 
         double amount = request.getAmount();
+        String account = request.getAccount();
         TransactionType transactionType = TransactionType.valueOf(request.getType().toUpperCase());
 
         if (amount <= 0) {
@@ -38,15 +44,17 @@ public class BankingService {
                 if (amount <= customer.getBalance()) {
                     customer.setBalance(customer.getBalance() - amount);
                 } else {
-                    throw new RuntimeException("Insufficient balance");
+                    throw new InsufficientBalanceException("Insufficient balance");
                 }
                 break;
             default:
-                throw new RuntimeException("Invalid transaction type, save or withdraw");
+                throw new InvalidTransactionTypeException("Invalid transaction type, save or withdraw");
 
         }
 
         customerRepository.save(customer);
+
+        messageService.sendMessage(customer, transactionType, amount, account);
 
         Banking transaction = Banking.builder()
                 .customer(customer)
@@ -59,14 +67,18 @@ public class BankingService {
         return bankingRepository.save(transaction);
     }
 
+    @Transactional
     public Banking transfer(BankingRequestDTO request, String receiverAccount) {
         Customer sender =
-                customerRepository.findByAccount(request.getAccount()).orElseThrow(() -> new RuntimeException(
-                        "Sender Account doesn't exist"));
-        Customer receiver = customerRepository.findByAccount(receiverAccount).orElseThrow(() -> new RuntimeException("Receiver Account doesn't exist"));
+                customerRepository.findByAccount(request.getAccount()).orElseThrow(() -> new AccountNotFoundException(
+                        "Sender's account not found"));
+        Customer receiver =
+                customerRepository.findByAccount(receiverAccount).orElseThrow(() -> new AccountNotFoundException("Receiver's " +
+                        "account not found"));
 
         double amount = request.getAmount();
         TransactionType transactionType = TransactionType.valueOf(request.getType().toUpperCase());
+        String account = request.getAccount();
 
         if (amount <= 0) {
             throw new IllegalArgumentException("Amount must be greater than 0");
@@ -74,14 +86,21 @@ public class BankingService {
 
         if (transactionType == TransactionType.TRANSFER) {
             if (amount >= sender.getBalance()) {
-                throw new RuntimeException("Insufficient balance");
+                throw new InsufficientBalanceException("Insufficient balance");
             }
 
             sender.setBalance(sender.getBalance() - amount);
             receiver.setBalance(receiver.getBalance() + amount);
+
+            customerRepository.save(sender);
+            customerRepository.save(receiver);
         } else {
-            throw new RuntimeException("Invalid transaction type, transfer only");
+            throw new InvalidTransactionTypeException("Invalid transaction type, transfer only");
         }
+
+
+        messageService.sendMessage(sender, transactionType, amount, account);
+
 
         Banking transaction = Banking.builder()
                 .customer(sender)
